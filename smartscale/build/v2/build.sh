d2fcf09aa29484153d200ef032da813b3ec5b9f0
rhine59 <rhine59@gmail.com>
2020-01-10 18:33:23 +0000

    Jan10
    Signed-off-by: rhine59 <rhine59@gmail.com>

---
+++ smartscale/build/v2/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+docker login --username=acmegrocery --password=5f47ecdd-a6cb-4a08-b99a-c03286e61954
+
+# Build and execute a simple NodeJS application that will respond to a CURL request to PORT 2020
+echo *INFO* finding and removing any previous instances
+id=`docker ps -a|grep -i scales|awk '{print $1}'`
+docker stop ${id}
+docker rm ${id}
+docker rmi acmegrocery/scales:v2
+echo *INFO* building a new NodeJS container image
+docker build -t acmegrocery/scales:v2 .
+sudo docker push acmegrocery/scales:v2
